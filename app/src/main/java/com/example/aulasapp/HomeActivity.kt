3772ebd0069dc33123ed7341ee5a047d06f3dc17
kikiymini <caramayo@fi.uba.ar>
2022-10-15 15:47:33 -0300

    Se agrega button logout

--- app/src/main/java/com/example/aulasapp/HomeActivity.kt
+++ app/src/main/java/com/example/aulasapp/HomeActivity.kt
@@ -2,10 +2,20 @@ package com.example.aulasapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var btn_logout = findViewById<Button>(R.id.logout)
+        ingresarHome(btn_logout);
+    }
+
+    private fun ingresarHome(logout:Button){
+        logout.setOnClickListener{
+            onBackPressed()
+        }
     }
 }
