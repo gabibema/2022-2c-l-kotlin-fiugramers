3b85a64ef79f726db2ee7bfe7fcf8f385b9fe48c
kikiymini <caramayo@fi.uba.ar>
2022-10-29 10:17:22 -0300

    Se agrega generarAulas comentado

--- app/src/main/java/com/example/aulasapp/HomeActivity.kt
+++ app/src/main/java/com/example/aulasapp/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.example.aulasapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +17,37 @@ class HomeActivity : AppCompatActivity() {
 
     private fun ingresarHome(logout:Button){
         logout.setOnClickListener{
+            //generarAulas() // genera las aulas en la bd
+            mostrarAulas()
             onBackPressed() // te devuelve a la ultima pantalla que estuviste
+        }
+    }
+
+    private fun mostrarAulas(){
+        val db = Firebase.firestore
+
+        db.collection("aulas")
+            .get()
+            .addOnSuccessListener { aulas ->
+                for(aula in aulas){
+                    println("AULA: ${aula.id} - ESTADO: ${aula.data.get("estado")}")
+                }
+            }.addOnFailureListener { exception ->
+                println("Error")
+            }
+    }
+
+    private fun generarAulas(){
+        val db = Firebase.firestore
+
+        for (i in 102..110){
+            val aula = hashMapOf(
+                "estado" to true
+            )
+
+            db.collection("aulas")
+                .document(i.toString())
+                .set(aula)
         }
     }
 }
